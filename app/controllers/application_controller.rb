b55e587194e50011c15613b3ed0b0257be91748f
Jenise Ruehle <jela07@gmail.com>
2020-10-17 20:20:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < Sinatra::Base
   end 
 
   post '/articles' do
-    @article = Article.create(params)
+    @article = Article.create(title: params[:title], content: params[:content])
     redirect to "/articles/#{@article.id}"
   end
   
